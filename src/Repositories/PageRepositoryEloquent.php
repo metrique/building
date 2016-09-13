05baf23b284dea6e1164f32c7d35b9e6eec2e75a
Daniel Hudson <danielhudson@shinobicorp.com>
2016-09-13 19:44:04 +0100

    Port Block Type controller

--- src/Repositories/PageRepositoryEloquent.php
+++ src/Repositories/PageRepositoryEloquent.php
@@ -27,18 +27,27 @@ class PageRepositoryEloquent implements PageRepositoryInterface
         return Page::orderBy('title', 'asc')->get(['id', 'title', 'slug', 'published']);
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function find($id)
     {
         return Page::find($id);
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function create(array $data)
     {
         $data['slug'] = $data['slug'] ?: $data['title'];
-        
+
         return Page::create($data);
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function createWithRequest()
     {
         return $this->create(request()->only([
@@ -50,16 +59,25 @@ class PageRepositoryEloquent implements PageRepositoryInterface
         ]));
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function destroy($id)
     {
         return Page::destroy($id);
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function update($id, array $data)
     {
         return $this->find($id)->update($data);
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function updateWithRequest($id)
     {
         return $this->update($id, request()->only([
