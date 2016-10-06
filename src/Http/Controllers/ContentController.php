509d6caeb7d6a79534d90c7c6821ba5fdedf15ff
Daniel Hudson <danielhudson@shinobicorp.com>
2016-10-06 08:54:16 +0100

    Test controller

---
+++ src/Http/Controllers/ContentController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Metrique\Building\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Metrique\Building\Http\Controllers\Controller;
+use Metrique\Building\Contracts\Page\ContentRepositoryInterface as Content;
+use Metrique\Building\Contracts\PageRepositoryInterface as Page;
+
+class ContentController extends Controller
+{
+    protected $views = [
+        'index' => 'metrique-building::content.index',
+    ];
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Page $page, Content $content)
+    {
+        $slug = $page->slugify(request()->path());
+
+        $this->mergeViewData([
+            'contents' => $page->contentBySlug($slug),
+            'page' => $page->bySlug($slug),
+            'slug' => $slug,
+        ]);
+
+        // dd($this->viewData);
+        return $this->viewWithData($this->views['index']);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
