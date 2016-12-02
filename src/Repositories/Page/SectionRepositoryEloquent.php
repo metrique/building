48d091f7b5e7d47c70523d69ff541caf2d243b39
Daniel Hudson <danielhudson@shinobicorp.com>
2016-12-02 22:49:22 +0000

    Fix unpublished feed items still shown.

--- src/Repositories/Page/SectionRepositoryEloquent.php
+++ src/Repositories/Page/SectionRepositoryEloquent.php
@@ -7,25 +7,25 @@ use Metrique\Building\Contracts\Page\SectionRepositoryInterface;
 
 class SectionRepositoryEloquent extends EloquentRepositoryAbstract implements SectionRepositoryInterface
 {
-	protected $modelClassName = 'Metrique\Building\Eloquent\Page\Section';
+    protected $modelClassName = 'Metrique\Building\Eloquent\Page\Section';
 
-	/**
-	 * {@inheritdocs}
-	 */
-	public function byPageId($id, $order = ['order' => 'desc'])
-	{
-		return $this->orderBy($order)->with(['block'])->where(['building_pages_id' => $id])->get()->toArray();
-	}
+    /**
+     * {@inheritdocs}
+     */
+    public function byPageId($id, $order = ['order' => 'desc'])
+    {
+        return $this->orderBy($order)->with(['block'])->where(['building_pages_id' => $id])->get()->toArray();
+    }
 
-	/**
-	 * {@inheritdocs}
-	 */
-	public function findWithAll($id)
-	{
-		$this->make();
+    /**
+     * {@inheritdocs}
+     */
+    public function findWithAll($id)
+    {
+        $this->make();
 
-		return $this->model->with(['page', 'block.structure' => function($query){
-			$query->orderBy('order', 'desc');
-		}, 'block.structure.type'])->where('id', $id)->first()->toArray();
-	}
+        return $this->model->with(['page', 'block.structure' => function ($query) {
+            $query->orderBy('order', 'desc');
+        }, 'block.structure.type'])->where('id', $id)->first()->toArray();
+    }
 }
