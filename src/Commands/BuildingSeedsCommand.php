ece7e892b5d95c7e83d3924218b18f927c0f45aa
Daniel Hudson <danielhudson@shinobicorp.com>
2017-05-18 08:04:31 +0100

    Fix namespace

--- src/Commands/BuildingSeedsCommand.php
+++ src/Commands/BuildingSeedsCommand.php
@@ -36,7 +36,7 @@ class BuildingSeedsCommand extends Command
      * @var array
      */
     protected $seeds = [
-        Metrique\Building\Database\Seeds\ComponentTypesSeeder::class,
+        \Metrique\Building\Database\Seeds\ComponentTypesSeeder::class,
     ];
 
     /**
