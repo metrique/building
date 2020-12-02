403802ef4c4eb8e609208fa4a9b94af320cd460b
Daniel Hudson <DanielHudson@users.noreply.github.com>
2020-12-02 22:32:51 +0000

    Add ServiceProvider

---
+++ src/BuildingServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Metrique\Building;
+
+use Illuminate\Support\ServiceProvider;
+
+class BuildingServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
