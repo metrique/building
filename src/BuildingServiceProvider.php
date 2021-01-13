e7940ccd97b69314362f7d451e63dfbd80e5becc
Daniel Hudson <DanielHudson@users.noreply.github.com>
2021-01-13 02:10:07 +0000

    Add `contents` migration

--- src/BuildingServiceProvider.php
+++ src/BuildingServiceProvider.php
@@ -16,7 +16,8 @@ class BuildingServiceProvider extends ServiceProvider
         if ($this->app->runningInConsole()) {
             if (!class_exists('CreatePagesTable')) {
                 $this->publishes([
-                    __DIR__ . '/../database/migrations/create_pages_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_pages_table.php'),
+                    __DIR__ . '/../database/migrations/create_pages_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '0_create_pages_table.php'),
+                    __DIR__ . '/../database/migrations/create_contents_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '1_create_contents_table.php'),
                 ], 'migrations');
             }
         }
