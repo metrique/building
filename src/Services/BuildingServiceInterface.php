4a48f9cac88632529614b4260776e6ecead9f673
Daniel Hudson <DanielHudson@users.noreply.github.com>
2021-01-14 23:06:13 +0000

    Add delete component from page feature

--- src/Services/BuildingServiceInterface.php
+++ src/Services/BuildingServiceInterface.php
@@ -8,5 +8,6 @@ use Metrique\Building\Support\Component;
 interface BuildingServiceInterface
 {
     public function addComponentToPage(Component $component, Page $page): bool;
+    public function deleteComponentFromPage(string $componentId, Page $page): bool;
     public function validateComponent(string $class): bool;
 }
---
+++ tests/Unit/DeleteContentTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Metrique\Building\Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Validator;
+use Metrique\Building\Http\Requests\PageRequest;
+use Metrique\Building\Tests\TestCase;
+use Metrique\Building\Models\Page;
+use Metrique\Building\Services\BuildingServiceInterface;
+use Metrique\Building\View\Components\TestComponent;
+use PHPUnit\Framework\Test;
+
+class DeleteContentTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_component_can_be_deleted_from_page_draft()
+    {
+        $building = resolve(BuildingServiceInterface::class);
+        $component = new TestComponent;
+        $page = Page::factory()->create();
+
+        $this->assertTrue(
+            $building->addComponentToPage($component, $page)
+        );
+        
+        $this->assertArrayHasKey(
+            $component->id(),
+            $page->draft
+        );
+
+        $this->assertTrue(
+            $building->deleteComponentFromPage($component->id(), $page)
+        );
+
+        $this->assertEmpty(
+            $page->draft
+        );
+    }
+}
