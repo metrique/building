8c162c8e70036849959859470f2233945a476350
Daniel Hudson <DanielHudson@users.noreply.github.com>
2021-02-20 11:54:54 +0000

    Handle multiple components in FormRequest

--- src/Http/Requests/ComponentEditRequest.php
+++ src/Http/Requests/ComponentEditRequest.php
@@ -3,10 +3,8 @@
 namespace Metrique\Building\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule;
 use Metrique\Building\Rules\ComponentIsBoundRule;
 use Metrique\Building\Services\BuildingServiceInterface;
-use Metrique\Building\Support\Component;
 
 class ComponentEditRequest extends FormRequest
 {
@@ -38,7 +36,12 @@ class ComponentEditRequest extends FormRequest
      */
     public function rules()
     {
-        return $this->fetchRules($this->_id);
+        return $this->fetchRules(
+            $this->request->get(
+                '_parent',
+                $this->request->get('_id')
+            )
+        );
     }
 
     public function prepareForValidation()
@@ -69,19 +72,23 @@ class ComponentEditRequest extends FormRequest
             );
         
         $rules = collect($component->rules());
-
-        if ($this->request->get('_type') == 'attributes') {
-            $rules = $rules->intersectByKeys(
+        
+        if ($this->request->get('_type', '') == 'attributes') {
+            return $rules->intersectByKeys(
                 $component->attributes()
-            );
+            )->toArray();
         }
 
-        if ($this->request->get('_type') == 'properties') {
-            $rules = $rules->intersectByKeys(
+        if ($this->request->get('_type', '') == 'multiple') {
+            return $rules->intersectByKeys(
+                collect($component->properties())->except('name')
+            )->toArray();
+        }
+
+        if ($this->request->get('_type', '') == 'properties') {
+            return $rules->intersectByKeys(
                 $component->properties()
-            );
+            )->toArray();
         }
-
-        return $rules->toArray();
     }
 }
